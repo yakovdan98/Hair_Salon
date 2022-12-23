35cce95bed3232a747dbe6eaf15145735676f66a
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-12-23 15:41:10 -0800

    added client creation page

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      if (client.StylistId == 0)
+      {
+        return RedirectToAction("Create");
+
+      }
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Stylists");
+    }
+  }  
+}
